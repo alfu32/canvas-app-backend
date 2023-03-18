ea753222a2e322a157c9e9394f3a5ae643eca5aa
alfu32 <ioan.alferaru@gmail.com>
2023-03-18 02:11:05 +0100

    refactor(trimIndent)

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,15 @@
 import express,{Express} from "express";
 import {config as testServiceConfig} from './test.service'
 import {config as entitiesServiceConfig} from './entities.service'
-import { ServiceConfigItemDescriptor, ServiceConfigResult } from "./meta";
-const app:Express = express();
-export function trimIndent(s:string):string{
-  const lines = s.split("\n")
-  const spaces=lines.reduce((min,line) => {
-    if(line === null){
-      return min
-    }else{
-      const match=line.match(/^\s+/gi)
-      if(match===null){
-        return 0
-      } else {
-        return Math.min(min,match[0].length)
-      }
-    }
-  },Number.MAX_SAFE_INTEGER)
-  return lines.map(line => line.substring(spaces)).join("\n")
-}
+import {config as apiServiceConfig} from './api.service'
+import { ServiceConfigResult } from "./meta";
 
+const app:Express = express();
 const configuredPaths={
   ...testServiceConfig(app),
   ...entitiesServiceConfig(app),
 } as ServiceConfigResult
+apiServiceConfig(app,configuredPaths)
 console.log(configuredPaths)
 
 if (process.env.PROD !== undefined){
