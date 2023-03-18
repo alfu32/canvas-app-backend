3dcc7591a0506aee80e8b0b612ab289f846ab92d
alfu32 <ioan.alferaru@gmail.com>
2023-03-18 02:27:37 +0100

    feat(scaffolding)

--- src/entities.service.ts
+++ src/entities.service.ts
@@ -2,5 +2,32 @@ import {Express} from "express";
 import { ServiceConfigResult } from "./meta";
 
 export function config(app:Express):ServiceConfigResult{
-    return {}
+    app.get("/entities",async (req,res)=>{
+        const response={
+            req:req.body,
+            date:new Date()
+        }
+        return response
+    })
+    app.post("/entities",(req,res)=>{
+
+        const response={
+            req:req.body,
+            date:new Date()
+        }
+        return response
+    })
+    return {
+
+        "POST /entities":{
+            method:"GET",
+            path:"/",
+            response:"string",
+        },
+        "GET /entities":{
+            method:"GET",
+            path:"/",
+            response:"string",
+        },
+    }
 }
