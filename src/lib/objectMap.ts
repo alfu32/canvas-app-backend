53e2ca5c84d1f74317a75a78fcb8673803e74ce8
alfu32 <ioan.alferaru@gmail.com>
2023-03-24 13:20:04 +0100

    refactor(common-lib)

--- src/lib/objectMap.ts
+++
@@ -1,7 +0,0 @@
-export function objectMap<T, R extends T>(obj: { [key: string]: T; }, mapper: (t: T, i?: number) => R): { [key: string]: R; } {
-  return Object.keys(obj).reduce((acc: { [key: string]: R; }, k: string, i?: number) => {
-    const t: T = obj[k];
-    acc[k] = mapper(t, i);
-    return acc;
-  }, {} as { [key: string]: R; });
-}
