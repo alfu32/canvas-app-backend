c5c8407078df65e66f6bb1dd77abf96730ea667f
alfu32 <ioan.alferaru@gmail.com>
2023-03-25 07:21:50 +0100

    feat(indexing)

--- server/routes.js
+++ server/routes.js
@@ -8,24 +8,66 @@ const router=new Router()
 router.route({method:/GET/gi,path:/^\/drawables$/gi,handler:(data)=>{
     for(let drawable of data){
         const dwString=JSON.stringify(drawable,null,' ')
+        let fdIndex=fs.openSync('drawables.json')
+        const len0=fs.fstatSync(fdIndex).size
+        fs.closeSync(fdIndex)
         fs.appendFileSync('drawables.json',dwString)
         fs.appendFileSync('drawables.json',"\n")
+        fdIndex=fs.openSync('drawables.json')
+        const len1=fs.fstatSync(fdIndex).size
+        fs.closeSync(fdIndex)
+
+        const indexData=JSON.stringify({
+            id:drawable.id,
+            len0,
+            len1,
+        },null,' ')
+        fs.appendFileSync('drawables.index.json',indexData)
+        fs.appendFileSync('drawables.index.json',"\n")
     }
     return data
 }})
 router.route({method:/POST|PUT|PATCH/gi,path:/^\/drawables$/gi,handler:(data)=>{
     for(let drawable of data){
         const dwString=JSON.stringify(drawable,null,' ')
+        let fdIndex=fs.openSync('drawables.json')
+        const len0=fs.fstatSync(fdIndex).size
+        fs.closeSync(fdIndex)
         fs.appendFileSync('drawables.json',dwString)
         fs.appendFileSync('drawables.json',"\n")
+        fdIndex=fs.openSync('drawables.json')
+        const len1=fs.fstatSync(fdIndex).size
+        fs.closeSync(fdIndex)
+
+        const indexData=JSON.stringify({
+            id:drawable.id,
+            len0,
+            len1,
+        },null,' ')
+        fs.appendFileSync('drawables.index.json',indexData)
+        fs.appendFileSync('drawables.index.json',"\n")
     }
     return data
 }})
 router.route({method:/DELETE/gi,path:/^\/drawables$/gi,handler:(data)=>{
     for(let drawable of data){
         const dwString=JSON.stringify(drawable,null,' ')
+        let fdIndex=fs.openSync('drawables.json')
+        const len0=fs.fstatSync(fdIndex).size
+        fs.closeSync(fdIndex)
         fs.appendFileSync('drawables.json',dwString)
         fs.appendFileSync('drawables.json',"\n")
+        fdIndex=fs.openSync('drawables.json')
+        const len1=fs.fstatSync(fdIndex).size
+        fs.closeSync(fdIndex)
+
+        const indexData=JSON.stringify({
+            id:drawable.id,
+            len0,
+            len1,
+        },null,' ')
+        fs.appendFileSync('drawables.index.json',indexData)
+        fs.appendFileSync('drawables.index.json',"\n")
     }
     return data
 }})
